217a15d79609dc6fb721363c448daec9cb47c47d
Tomohiko Ozawa <tomohiko.ozawa@sony.com>
2019-01-16 05:52:52 +0900

    hello

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import json
+
 from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return Response(response=json.dumps({'hello': 'world'}),
+                    status=200,
+                    mimetype="application/json")
+
+
 @app.route('/post', methods=["POST"])
 def echo():
+    print(request.data)
     return Response(response=request.data.decode('utf-8'),
                     status=200,
                     mimetype="application/json")
