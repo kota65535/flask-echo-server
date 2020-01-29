c6651ef34f0ad960316042e44fa2d5211ef579df
Tomohiko Ozawa <tomohiko.ozawa@sony.com>
2020-01-29 14:33:18 +0900

    envoy

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 import json
-
+import time
 from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return Response(response=json.dumps({'hello': 'world'}),
                     status=200,
                     mimetype="application/json")
 
-
-@app.route('/post', methods=["POST"])
-def echo():
-    print(request.json)
-    return Response(response=json.dumps(request.json),
-                    status=200,
-                    mimetype="application/json")
-
-
-@app.route("/post/<status>", methods=["POST"])
-def echo_with_status(status):
-    return Response(response=json.dumps(request.json),
-                    status=status,
-                    mimetype="application/json")
+#
+# @app.route('/post', methods=["POST"])
+# def echo():
+#     print(request.json)
+#     return Response(response=json.dumps(request.json),
+#                     status=200,
+#                     mimetype="application/json")
+#
+#
+# @app.route("/post/<status>", methods=["POST"])
+# def echo_with_status(status):
+#     if status == '0':
+#         time.sleep(10)
+#     return Response(response=json.dumps(request.json),
+#                     status=status,
+#                     mimetype="application/json")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
